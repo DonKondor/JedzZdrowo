a55fc5c081a28ce7540d08a08a21ad52c402327b
DonKondor <44501845+DonKondor@users.noreply.github.com>
2018-10-30 16:00:05 +0100

    Add files via upload
    Początki mojej gierki

---
+++ src/jedzzdrowo/Okno.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jedzzdrowo;
+import java.awt.*;
+/**
+ *
+ * @author Konrad
+ */
+    class Okno extends Frame{
+Okno(String nazwa){
+super(nazwa); //metoda super wywołuje konstruktor nadklasy
+setResizable(false);
+setSize(400,400);
+}
+}
